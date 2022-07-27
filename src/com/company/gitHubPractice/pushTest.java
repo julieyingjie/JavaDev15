daf619b9485cc40661938cf1edb85e4eb11a665f
julieyingjie <julieyingjie@gmail.com>
2022-07-26 22:54:02 -0400

    This is the 1st commit under the 1 time pushing test

---
+++ src/com/company/gitHubPractice/pushTest.java
@@ -0,0 +1,7 @@
+package com.company.gitHubPractice;
+
+public class pushTest {
+    public static void main(String[] args) {
+        System.out.println("This is only a push test for pushing several commits at one time");
+    }
+}
