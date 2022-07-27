584964150db6c6099d281ab72353e653da5c8073
julieyingjie <julieyingjie@gmail.com>
2022-07-26 22:55:08 -0400

    This is the 2nd commit under the 1 time pushing test

---
+++ src/com/company/gitHubPractice/pushTest2.java
@@ -0,0 +1,7 @@
+package com.company.gitHubPractice;
+
+public class pushTest2 {
+    public static void main(String[] args) {
+        System.out.println("This is the 2nd commit of the pushing several commits at one time.");
+    }
+}
